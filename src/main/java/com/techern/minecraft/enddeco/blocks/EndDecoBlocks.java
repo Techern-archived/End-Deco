4ae5b211ebd016d7ead667e7dd000109990ed5a2
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-03-17 14:54:42 +1000

    Add some javadoc and set brick harvest level|tool

--- src/main/java/com/techern/minecraft/enddeco/blocks/EndDecoBlocks.java
+++ src/main/java/com/techern/minecraft/enddeco/blocks/EndDecoBlocks.java
@@ -15,17 +15,30 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
  */
 public class EndDecoBlocks {
 
+    /**
+     * A {@link Block} defining an end stone brick
+     *
+     * @since 0.0.1
+     */
     public static Block END_STONE_BRICKS = new Block(Material.rock, MapColor.sandColor).setHardness(0.8f)
                                                                    .setResistance(4f)
                                                                    .setCreativeTab(CreativeTabs.tabDecorations)
                                                                    .setUnlocalizedName("end_stone_bricks")
                                                                    .setStepSound(Block.soundTypePiston); /*what*/
 
+    /**
+     * Registers the {@link Block}s added by the {@link EndDecoMod}
+     *
+     * @since 0.0.1
+     */
     public static void registerBlocks() {
 
         //First, let's check and do end stone bricks
 
         if (EndDecoMod.CONFIGURATION.getBoolean("End_Stone_Bricks", "NEW_BLOCKS", true, "Enables the use of end stone bricks")) {
+
+            //Now we have to set the level required for mining
+            END_STONE_BRICKS.setHarvestLevel("pickaxe", 0);
             GameRegistry.registerBlock(END_STONE_BRICKS, "end_stone_bricks");
         }
 
