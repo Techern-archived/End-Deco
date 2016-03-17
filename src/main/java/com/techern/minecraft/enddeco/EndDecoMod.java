fc72c75a2d4510936202f7ca461376f3ea707f40
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-03-17 12:00:35 +1000

    Initial dump, contains an almost complete implementation of end stone bricks
    Refers to #1

---
+++ src/main/java/com/techern/minecraft/enddeco/EndDecoMod.java
@@ -0,0 +1,95 @@
+package com.techern.minecraft.enddeco;
+
+import com.techern.minecraft.enddeco.blocks.EndDecoBlocks;
+import com.techern.minecraft.enddeco.proxy.CommonProxy;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * A {@link net.minecraftforge.fml.common.Mod} for decorative (and functional) blocks in the end
+ *
+ * @since 0.0.1
+ */
+@Mod(modid = "end_deco", version = EndDecoMod.VERSION)
+public class EndDecoMod {
+
+    /**
+     * The version {@link String}
+     *
+     * @since 0.0.1
+     */
+    public static final String VERSION = "0.0.1-dev";
+
+    /**
+     * The {@link Logger} for this mod
+     *
+     * Gets replaced in {@link EndDecoMod#handlePreInitEvent(FMLPreInitializationEvent)}
+     *
+     * @since 0.0.1
+     */
+    public static Logger LOGGER = LogManager.getLogger(EndDecoMod.class);
+
+    /**
+     * A {@link SidedProxy} of either {@link CommonProxy} or {@link com.techern.minecraft.enddeco.proxy.ClientProxy}
+     *
+     * @since 0.0.1
+     */
+    @SidedProxy(clientSide = "com.techern.minecraft.enddeco.proxy.ClientProxy",
+            serverSide = "com.techern.minecraft.enddeco.proxy.CommonProxy")
+    public static CommonProxy PROXY;
+
+    /**
+     * The {@link Configuration} (backed by a {@link java.io.File} used by {@link EndDecoMod}
+     *
+     * @since 0.0.1
+     */
+    public static Configuration CONFIGURATION;
+
+    /**
+     * Handles the {@link FMLPreInitializationEvent}
+     *
+     * @param event The {@link FMLPreInitializationEvent}
+     * @since 0.0.1
+     */
+    @Mod.EventHandler
+    public void handlePreInitEvent(FMLPreInitializationEvent event) {
+        LOGGER = event.getModLog();
+        LOGGER.debug("Attempting to load configuration file...");
+
+        CONFIGURATION = new Configuration(event.getSuggestedConfigurationFile());
+
+        LOGGER.info("Loaded configuration file for End Deco");
+    }
+
+    /**
+     * Handles the {@link FMLInitializationEvent}
+     *
+     * @param event The {@link FMLInitializationEvent}
+     * @since 0.0.1
+     */
+    @Mod.EventHandler
+    public void handleInitEvent(FMLInitializationEvent event) {
+        EndDecoBlocks.registerBlocks();
+    }
+
+    /**
+     * Handles the {@link FMLPostInitializationEvent}
+     *
+     * @param event The {@link FMLPostInitializationEvent}
+     * @since 0.0.1
+     */
+    @Mod.EventHandler
+    public void handlePostInitEvent(FMLPostInitializationEvent event) {
+        //Register recipes here
+        if (CONFIGURATION.hasChanged()) {
+            LOGGER.info("End Deco found extra / changed configuration, and is now saving");
+            CONFIGURATION.save();
+        }
+    }
+}
---
+++ src/main/java/com/techern/minecraft/enddeco/blocks/EndDecoBlocks.java
@@ -0,0 +1,45 @@
+package com.techern.minecraft.enddeco.blocks;
+
+import com.techern.minecraft.enddeco.EndDecoMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+/**
+ * A utility class that registers and creates each new {@link net.minecraft.block.Block} for the {@link com.techern.minecraft.enddeco.EndDecoMod}
+ *
+ * @since 0.0.1
+ */
+public class EndDecoBlocks {
+
+    public static Block END_STONE_BRICKS = new Block(Material.rock, MapColor.sandColor).setHardness(0.8f)
+                                                                   .setResistance(4f)
+                                                                   .setCreativeTab(CreativeTabs.tabDecorations)
+                                                                   .setUnlocalizedName("end_stone_bricks")
+                                                                   .setStepSound(Block.soundTypePiston); /*what*/
+
+    public static void registerBlocks() {
+
+        //First, let's check and do end stone bricks
+
+        if (EndDecoMod.CONFIGURATION.getBoolean("End_Stone_Bricks", "NEW_BLOCKS", true, "Enables the use of end stone bricks")) {
+            GameRegistry.registerBlock(END_STONE_BRICKS, "end_stone_bricks");
+        }
+
+        //Register other blocks later
+
+        //Now let's do proxy shit
+
+        if (EndDecoMod.CONFIGURATION.getBoolean("End_Stone_Bricks", "NEW_BLOCKS", true, "Enables the use of end stone bricks")) {
+            EndDecoMod.PROXY.registerItemModelMesher(Item.getItemFromBlock(END_STONE_BRICKS), 0, "end_stone_bricks", "inventory");
+        }
+
+
+    }
+
+
+
+}
